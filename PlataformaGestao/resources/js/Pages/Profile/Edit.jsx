24f38857a0818ab0cddaa843d82650f1649a9851
josepinho22 <pinho.jose25@gmail.com>
2026-03-02 16:19:28 +0000

    Update Edit.jsx

--- PlataformaGestao/resources/js/Pages/Profile/Edit.jsx
+++ PlataformaGestao/resources/js/Pages/Profile/Edit.jsx
@@ -28,8 +28,6 @@ export default function Edit({ mustVerifyEmail, status }) {
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
                         <UpdatePasswordForm className="max-w-xl" />
                     </div>
-
-
                 </div>
             </div>
         </AuthenticatedLayout>
